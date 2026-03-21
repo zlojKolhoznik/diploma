69622f47e0e2f1c464b0a9c701f616554d46177c
Roman Portianko <rportyanko10@gmail.com>
2026-03-21 21:33:17 +0200

    feat: add Dish entity and IDishRepository interface

---
+++ RestaurantWithAi.Core/Contracts/IDishRepository.cs
@@ -0,0 +1,12 @@
+﻿using RestaurantWithAi.Core.Entities;
+
+namespace RestaurantWithAi.Core.Contracts;
+
+public interface IDishRepository
+{
+    Task<IEnumerable<Dish>> GetAllDishesAsync();
+    Task<Dish> GetDishByIdAsync(Guid id);
+    Task AddDishAsync(Dish dish);
+    Task UpdateDishAsync(Dish dish);
+    Task DeleteDishAsync(Guid id);
+}
---
+++ RestaurantWithAi.Core/Entities/Dish.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
+
+namespace RestaurantWithAi.Core.Entities;
+
+[ExcludeFromCodeCoverage]
+[DataContract]
+public class Dish
+{
+    [Key]
+    [DataMember]
+    public Guid Id { get; set; }
+    
+    [DataMember]
+    [Required]
+    public required string Name { get; set; }
+    
+    [DataMember]
+    [Required]
+    public required string Description { get; set; }
+    
+    [DataMember]
+    [Required]
+    public decimal Price { get; set; }
+    
+    [DataMember]
+    [Required]
+    public required string ImageUrl { get; set; }
+}
